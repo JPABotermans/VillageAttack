139a4061a7fe9c53659fc18df7fa69e1a18b9de7
AlexOdyn <gamerovich96@gmail.com>
2020-12-13 15:28:16 +0100

    Added a path that the player will follow. Add vertices to the path by right clicking on the scene.

--- unity/Assets/VillageAttack/Scripts/MoveArmy.cs
+++ unity/Assets/VillageAttack/Scripts/MoveArmy.cs
@@ -1,37 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Util.Geometry.Graph;
 
 public class MoveArmy : MonoBehaviour {
 
-	private float movementX = 3f;
-	private float movementY = 3f;
+	private Vector2 speed = new Vector2(3f, 3f);
 	private Vector3 startPos = new Vector3(-11f, -3.5f, -10f);
 	private Rigidbody2D _body;
 	private BoxCollider2D _box;
+	private Material m_LineMaterial;
+	public List<Vertex> path = new List<Vertex>();
+	private static float epsilon = 0.5f;
+
 	// Use this for initialization
 	void Start () {
 		_body = GetComponent<Rigidbody2D>();
 		_box = GetComponent<BoxCollider2D>();
 	}
-	
+
+	void Awake()
+	{
+		Shader shader = Shader.Find("Hidden/Internal-Colored");
+		m_LineMaterial = new Material(shader)
+		{
+			hideFlags = HideFlags.HideAndDontSave
+		};
+		m_LineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+		m_LineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+		m_LineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+		m_LineMaterial.SetInt("_ZWrite", 0);
+	}
+
 	// Update is called once per frame
-	void Update () {
-		float deltaX = Input.GetAxis("Horizontal") * movementX;
-		float deltaY = Input.GetAxis("Vertical") * movementY;
+	void Update() {
+		float deltaX = Input.GetAxis("Horizontal") * speed.x;
+		float deltaY = Input.GetAxis("Vertical") * speed.y;
+
+		// Follow a path
+		Vertex firstOnPath = path.Count > 0 ? path[0] : null;
+		if (firstOnPath != null)
+        {
+			if (Vector2.Distance((Vector2)_body.transform.position, firstOnPath.Pos) <= epsilon)
+			{
+				path.RemoveAt(0);
+			}
+			else
+			{
+				deltaX =  firstOnPath.Pos.x - _body.transform.position.x;
+				deltaY =  firstOnPath.Pos.y - _body.transform.position.y;
+			}
+		}
+
 		Vector2 movement = new Vector2(deltaX, deltaY);
+		movement.Normalize();
+		movement.Scale(speed);
 		_body.velocity = movement;
-		
+
+		DrawPath();
 		// Debug.Log(village_collision);
 		// if (village_collision != null){
-			
+
 		// 	Debug.Log("we have won");
 		// }
 		// // Debug.Log("We are moving to " + startPos);dw
-		
+
 	}
 
+	private void OnRenderObject()
+	{
+		// Apply the line material
+		m_LineMaterial.SetPass(0);
+		DrawPath();
+	}
 
+	private void DrawPath()
+    {
+		if (path.Count == 0) return;
+		GL.Begin(GL.LINE_STRIP);
+		GL.Color(Color.red);
+		GL.Vertex(_body.transform.position);
+		foreach (Vertex v in path)
+        {
+			Debug.Log("Drawing line");
+			GL.Vertex(new Vector3(v.Pos.x, v.Pos.y, 0));
+		}
+		GL.End();
+    }
 	// void OnCollisionEnter2D(Collision2D col) {
 	// 	if (col.collider.name =="Mountain"){
 	// 		Debug.Log("You hit the mountain");
--- unity/Assets/VillageAttack/Scripts/SceneController.cs
+++ unity/Assets/VillageAttack/Scripts/SceneController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine;
+using Util.Geometry.Graph;
 
 public class SceneController : MonoBehaviour {
 	[SerializeField] public GameObject[] MountainPrefabs;
@@ -19,24 +20,42 @@ public class SceneController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)){
+		if (Input.GetMouseButtonDown(0))
+		{
 			Vector3 pos = Input.mousePosition;
 			Debug.Log("We are clicking somewhere" + pos);
 			Vector3 pos_world = Camera.main.ScreenToWorldPoint(
-						new Vector3(Input.mousePosition.x, 
-						Input.mousePosition.y, 
+						new Vector3(Input.mousePosition.x,
+						Input.mousePosition.y,
 						Camera.main.nearClipPlane)
 						);
-			
-			if (pos_world[0]> -15)
-			{
-			_mountain = Instantiate(MountainPrefabs[this._current_prefab]) as GameObject;
-			_mountain.transform.position = new Vector3(pos_world[0], pos_world[1], pos_world[2]);
-			// _mountain.transform.localScale = new Vector3(1, 1,);
-			}
-			
-			
+			CreateMountain(pos_world);
+		} else if (Input.GetMouseButtonUp(1)) {
+			Vector3 pos = Input.mousePosition;
+			Vector3 pos_world = Camera.main.ScreenToWorldPoint(
+						new Vector3(Input.mousePosition.x,
+						Input.mousePosition.y,
+						Camera.main.nearClipPlane)
+						);
+			AddNewPathVertex(pos_world);
+		}
+	}
 
+	private void AddNewPathVertex(Vector3 worldPosition)
+    {
+		Vertex vertex = new Vertex(worldPosition.x, worldPosition.y);
+		GameObject army = GameObject.FindGameObjectsWithTag("Player")[0];
+		Debug.Log(army.name);
+		MoveArmy armyComponent = army.GetComponent<MoveArmy>();
+		armyComponent.path.Add(vertex);
+    }
+
+	private void CreateMountain(Vector3 worldPosition)
+    {
+		if (worldPosition[0] > -15)
+		{
+			_mountain = Instantiate(MountainPrefabs[this._current_prefab]) as GameObject;
+			_mountain.transform.position = new Vector3(worldPosition[0], worldPosition[1], worldPosition[2]);
 		}
 	}
 }
