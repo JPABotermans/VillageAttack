9a18a925fc6cb5098db2ec7a9f5b3c382d03069b
wouterwln <wouternuijten@gmail.com>
2020-12-21 14:01:48 +0100

    Implemented naive visibilitygraph

---
+++ unity/Assets/Scripts/Editor/Tests/DataStructures/VisibilityGraph/VisibilityGraphTest.cs
@@ -0,0 +1,26 @@
+﻿namespace Util.VisibilityGraph.Tests
+{
+    using UnityEngine;
+    using UnityEditor;
+    using NUnit.Framework;
+    using System;
+    using System.Linq;
+
+
+    [TestFixture]
+    public class VisibilityGraphTest
+    {
+        private VisibilityGraph visibilityGraph;
+        
+        private void Construct()
+        {
+
+        }
+
+        [Test]
+        private void isTested()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
