fb004b7f67e55c5b1e7ea481c210d4bd5f5161e6
wouterwln <wouternuijten@gmail.com>
2020-12-21 14:24:34 +0100

    VisibilityGraph v0.1

--- unity/Assets/Scripts/Editor/Tests/DataStructures/VisibilityGraph/VisibilityGraphTest.cs
+++ unity/Assets/Scripts/Editor/Tests/DataStructures/VisibilityGraph/VisibilityGraphTest.cs
@@ -5,6 +5,9 @@
     using NUnit.Framework;
     using System;
     using System.Linq;
+    using Util.Geometry.Polygon;
+    using System.Collections;
+    using System.Collections.Generic;
 
 
     [TestFixture]
@@ -14,13 +17,26 @@
         
         private void Construct()
         {
-
+            LinkedList<Polygon2D> polygons = new LinkedList<Polygon2D>();
+            for(int i = 1; i < 10; i++)
+            {
+                LinkedList<Vector2> vertices = new LinkedList<Vector2>();
+                vertices.AddLast(new Vector2(i * 5, i * i * 5));
+                vertices.AddLast(new Vector2((i * 5) + 1, i * i * 5));
+                vertices.AddLast(new Vector2((i * 5) + 1, (i * i * 5) + 1));
+                vertices.AddLast(new Vector2(i * 5, (i * i * 5) + 1));
+                Polygon2D p = new Polygon2D(vertices);
+                polygons.AddLast(p);
+            }
+            Vector2 origin = new Vector2(0, 0);
+            Vector2 target = new Vector2(10, 0);
+            visibilityGraph = new VisibilityGraph(polygons, origin, target);
         }
 
         [Test]
-        private void isTested()
+        public void isTested()
         {
-            Assert.IsTrue(true);
+            Construct();
         }
     }
 }
---
+++ unity/Assets/Scripts/Util/DataStructures/VisilbilityGraph/VisibilityGraph.cs
@@ -0,0 +1,86 @@
+﻿namespace Util.VisibilityGraph
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using Util.Geometry;
+    using UnityEngine;
+    using Util.Geometry.Polygon;
+    using Util.Geometry.Graph;
+
+    public class VisibilityGraph
+    {
+        private LinkedList<Vertex> m_vertices;
+        private LinkedList<Polygon2D> m_polygons;
+        private AdjacencyListGraph g;
+
+        public VisibilityGraph(LinkedList<Polygon2D> polygons, Vector2 control_point, Vector2 village)
+        {
+            m_polygons = polygons;
+            m_vertices = new LinkedList<Vertex>();
+            m_vertices.AddLast(new Vertex(control_point));
+            m_vertices.AddLast(new Vertex(village));
+            foreach (Polygon2D p in polygons)
+            {
+                foreach (Vector2 v in p.Vertices)
+                {
+                    Vertex point = new Vertex(v);
+                    m_vertices.AddLast(point);
+                }
+            }
+            LinkedList<Edge> edges = new LinkedList<Edge>();
+            foreach(Vertex origin in m_vertices)
+            {
+                LinkedList<Vertex> visible_vertices = this.VisibleVertices(origin);
+                foreach(Vertex target in visible_vertices)
+                {
+                    float weight = (origin.Pos - target.Pos).magnitude;
+                    Edge e = new Edge(origin, target, weight);
+                    edges.AddLast(e);
+                }
+            }
+            g = new AdjacencyListGraph(m_vertices, edges);
+        }
+
+        private LinkedList<Vertex> VisibleVertices(Vertex v)
+        {
+            LinkedList<Vertex> result = new LinkedList<Vertex>();
+            foreach(Vertex p in this.m_vertices) { 
+                if(this.IsVisible(v, p))
+                {
+                    result.AddLast(p);
+                }
+                
+            }
+            return result;
+        }
+
+        private bool IsVisible(Vertex p, Vertex v)
+        {
+            if (p.Pos.Equals(v.Pos))
+            {
+                return false;
+            }
+            LineSegment seg = new LineSegment(p.Pos, v.Pos);
+            foreach(Polygon2D poly in this.m_polygons)
+            {
+                if(poly.OnBoundary(p.Pos) && poly.OnBoundary(v.Pos) && poly.Contains((v.Pos + p.Pos) / 2))
+                {
+                    return false;
+                }
+                foreach(LineSegment intersecting_segment in poly.Segments)
+                {
+                    Vector2? intersecting_point = seg.Intersect(intersecting_segment);
+                    if (intersecting_point != null)
+                    {
+                        if(!(intersecting_point.Equals(v.Pos) || intersecting_point.Equals(p.Pos)))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+}
